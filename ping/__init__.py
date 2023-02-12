64060e5df57e3b982d866d7639a2c72787ac4a41
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2023-02-12 15:02:30 +0200

    Add documentation.

---
+++ ping/__init__.py
@@ -0,0 +1,3 @@
+__author__ = """Hristo Georgiev"""
+__email__ = 'hristo.i.georgiev@gmail.com'
+__version__ = '0.1.0'
--- ping/ping/ping.py
+++ ping/ping/ping.py
@@ -6,8 +6,15 @@ LOGGER = logging.getLogger(__name__)
 
 
 class FileService:
+    """
+    Service for writing message to a file.
+    """
     @property
-    def message(self):
+    def message(self) -> str:
+        """
+        Message, which is witten on the file.
+        :return: ste
+        """
         if self._message is None:
             self._message = "Hello"
         return self._message
@@ -15,7 +22,12 @@ class FileService:
     def __init__(self, message=None):
         self._message = message
 
-    def run(self, path):
+    def run(self, path) -> None:
+        """
+        Run the service using a specified file,
+        :param path: str
+        :return: str
+        """
         LOGGER.info("Writing '{}' to file {}.".format(self.message, path))
 
         with open(path, "w") as fp:
@@ -23,8 +35,16 @@ class FileService:
 
 
 class PingManager:
+    """
+    Manager that writes a message to a file, then pings an IRL and get the contents of the URL.
+    """
+
     @property
-    def file_service(self):
+    def file_service(self) -> FileService:
+        """
+        Get the FileService used by the manager.
+        :return: FileService
+        """
         if self._file_service is None:
             self._file_service = FileService()
         return self._file_service
@@ -32,6 +52,13 @@ class PingManager:
     def __init__(self, file_service=None):
         self._file_service = file_service
 
-    def run(self, path, url):
+    def run(self, path, url) -> None:
+        """
+        Run the manager with specific 'path' and 'url'.
+        :param path: str
+        :param url: str
+        :return: None
+        """
+
         LOGGER.info("Running manager ...")
         self.file_service.run(path)
